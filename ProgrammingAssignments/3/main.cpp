edcb1026ca1d2fcd5f4078f776c940e9526e0fba
jadecss <ttjit963@gmail.com>
2022-04-09 15:31:22 +0000

    init

---
+++ ProgrammingAssignments/3/main.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <iostream>
+using namespace std;
+
+namespace ProgrammingAssignments
+{
+    class Decompressor
+    {
+        private:
+        int alphabetSize;
+        int codeSize;
+        map<char, bool*> alphabet;
+        map<char, int> frequency;
+
+        public:
+        void readInput()
+        {
+            
+        }
+    };
+}
+
+
+int main()
+{
+
+}
